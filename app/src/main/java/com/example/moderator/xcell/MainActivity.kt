33f150d71f7162a6e4b8315465c80082f91f52bd
allaBek <mohammed.sayah.bekkoucha@gmail.com>
2019-03-04 12:32:11 +0200

    updated colors

--- app/src/main/java/com/example/moderator/xcell/MainActivity.kt
+++ app/src/main/java/com/example/moderator/xcell/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.light_control)
+        setContentView(R.layout.devices)
     }
 }
