68f16944bf3ed09a7017b50363c1dd8db145f3d2
Valtteri Erkkila <valtteri.erkkila@gmail.com>
2019-04-18 13:21:53 +0300

    Remove wifi p2p use normal wifi service discovery

--- app/src/main/java/com/example/moderator/xcell/WelcomeScreen.java
+++ app/src/main/java/com/example/moderator/xcell/WelcomeScreen.java
@@ -4,18 +4,113 @@ import android.Manifest;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class WelcomeScreen extends AppCompatActivity {
     public static final String TAG = WelcomeScreen.class.getSimpleName();
     private int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 0;
     private int PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION = 1;
     private int PERMISSIONS_REQUEST_CODE_CHANGE_WIFI_STATE = 2;
+
+    private NsdServiceInfo mService;
+    private NsdManager nsdManager;
+
+    public void startServiceDiscovery() {
+
+        nsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
+        // Instantiate a new DiscoveryListener
+
+        final NsdManager.ResolveListener resolveListener = new NsdManager.ResolveListener() {
+
+            @Override
+            public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Called when the resolve fails. Use the error code to debug.
+                Log.e(TAG, "Resolve failed: " + errorCode);
+            }
+
+            @Override
+            public void onServiceResolved(NsdServiceInfo serviceInfo) {
+                Log.e(TAG, "Resolve Succeeded. " + serviceInfo);
+
+                if (serviceInfo.getServiceName().equals("asd")) {
+                    Log.d(TAG, "Same IP.");
+                    return;
+                }
+                mService = serviceInfo;
+                int port = mService.getPort();
+                InetAddress host = mService.getHost();
+                try {
+                    Socket socket = new Socket(host, port);
+                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    out.write("TstMsg");
+                } catch (UnknownHostException e) {
+
+                } catch (IOException e) {
+
+                }
+            }
+        };
+
+        NsdManager.DiscoveryListener discoveryListener = new NsdManager.DiscoveryListener() {
+
+            // Called as soon as service discovery begins.
+            @Override
+            public void onDiscoveryStarted(String regType) {
+                Log.d(TAG, "Service discovery started");
+            }
+
+            @Override
+            public void onServiceFound(NsdServiceInfo service) {
+                // A service was found! Do something with it.
+                Log.d(TAG, "Service discovery success" + service);
+                if (service.getServiceName().contains("xcell")){
+                    nsdManager.resolveService(service, resolveListener);
+                }
+            }
+
+            @Override
+            public void onServiceLost(NsdServiceInfo service) {
+                // When the network service is no longer available.
+                // Internal bookkeeping code goes here.
+                Log.e(TAG, "service lost: " + service);
+            }
+
+            @Override
+            public void onDiscoveryStopped(String serviceType) {
+                Log.i(TAG, "Discovery stopped: " + serviceType);
+            }
+
+            @Override
+            public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+                Log.e(TAG, "Discovery failed: Error code:" + errorCode);
+                nsdManager.stopServiceDiscovery(this);
+            }
+
+            @Override
+            public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+                Log.e(TAG, "Discovery failed: Error code:" + errorCode);
+                nsdManager.stopServiceDiscovery(this);
+            }
+        };
+
+        nsdManager.discoverServices(
+                "_xcell._tcp.", NsdManager.PROTOCOL_DNS_SD, discoveryListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,22 +131,6 @@ public class WelcomeScreen extends AppCompatActivity {
         }else{
             //do something, permission was previously granted; or legacy device
         }
-        scheduleJob();
-    }
-
-
-    private void scheduleJob(){
-        ComponentName wifiJobService = new ComponentName(getApplicationContext(), WiFiJobService.class);
-        JobInfo jobInfo = new JobInfo.Builder(8,wifiJobService)
-                .setRequiresCharging(false)
-                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                .build();
-        JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
-        int res = jobScheduler.schedule(jobInfo);
-        if (res == JobScheduler.RESULT_SUCCESS){
-            Log.i(TAG, "Jobscheduled");
-        } else {
-            Log.i(TAG,"Failed");
-        }
+        startServiceDiscovery();
     }
 }
